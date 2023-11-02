043b6eb04aa976ae528487db90f8c18056d39e5b
Rodgers Chayuga <rodgers@food4education.org>
2023-11-02 19:22:05 +0300

    feat: install shadcn advanced table component

---
+++ src/components/ui/table/types/index.ts
@@ -0,0 +1,21 @@
+export type Option = {
+  label: string;
+  value: string;
+  icon?: React.ComponentType<{ className?: string }>;
+};
+
+export interface DataTableFilterOption<TData> {
+  label: string;
+  value: keyof TData;
+  items: Option[];
+}
+
+export interface DataTableSearchableColumn<TData> {
+  id: keyof TData;
+  title: string;
+}
+
+export interface DataTableFilterableColumn<TData>
+  extends DataTableSearchableColumn<TData> {
+  options: Option[];
+}
